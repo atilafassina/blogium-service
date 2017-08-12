057bbf2f463debbe3299b145d5ae6215a1d46d4f
Atila Fassina <hey@atilafassina.com>
2017-08-12 22:08:42 +0200

    fix xo config / format files

--- index.js
+++ index.js
@@ -3,32 +3,32 @@ const {router, get} = require('microrouter')
 const rss = require('simple-rss')
 
 async function getPosts(user = 'medium') {
-	const mediumList = await rss(`https://medium.com/feed/@${user}`)
-	const dataCleanup = await sanitizePostList(mediumList)
-	return onlyPosts(dataCleanup)
+  const mediumList = await rss(`https://medium.com/feed/@${user}`)
+  const dataCleanup = await sanitizePostList(mediumList)
+  return onlyPosts(dataCleanup)
 }
 
 function sanitizePostList(jsonFeed) {
-	return jsonFeed.map(({title, date, categories, link}) => {
-		return {
-			title,
-			date,
-			link,
-			categories
-		}
-	})
+  return jsonFeed.map(({title, date, categories, link}) => {
+    return {
+      title,
+      date,
+      link,
+      categories
+    }
+  })
 }
 
 function onlyPosts(list) {
-	return list.filter(({categories}) => categories.length > 0)
+  return list.filter(({categories}) => categories.length > 0)
 }
 
 const posts = async (req, res) => {
-	const posts = await getPosts(req.params.user)
+  const posts = await getPosts(req.params.user)
 
-	res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
-	return send(res, 200, posts)
+  return send(res, 200, posts)
 }
 
 const notfound = (req, res) => send(res, 404, 'Not found route')
@@ -39,7 +39,7 @@ module.exports = router(
 )
 
 module.exports.privates = {
-	_getPosts: getPosts,
-	_sanitizePostList: sanitizePostList,
-	_onlyPosts: onlyPosts
+  _getPosts: getPosts,
+  _sanitizePostList: sanitizePostList,
+  _onlyPosts: onlyPosts
 }
--- index.test.js
+++ index.test.js
@@ -2,38 +2,38 @@ import test from 'ava'
 import {privates} from './'
 
 test('filter comments out of feed', t => {
-	const mockArray = [
+  const mockArray = [
     {name: 'foo', categories: []},
     {name: 'foo', categories: [4, 5, 6, 3]},
     {name: 'foo', categories: []},
     {name: 'foo', categories: [1, 2, 3]}
-	]
+  ]
 
-	t.is(privates._onlyPosts(mockArray).length, 2, 'filtered only posts with categories')
+  t.is(privates._onlyPosts(mockArray).length, 2, 'filtered only posts with categories')
 })
 
 test('check sanitizePostList', t => {
-	const mockArray = [{
-		title: 'mock title',
-		date: '2022-01-01 17:14:14',
-		link: 'https://medium.com/@foo',
-		guid: 'https://medium.com/p/foohash',
-		author: 'Foobar da Silva',
-		thumbnail: '',
-		description: 'mock description',
-		content: 'foo content',
-		enclosure: [],
-		categories: []
-	}]
+  const mockArray = [{
+    title: 'mock title',
+    date: '2022-01-01 17:14:14',
+    link: 'https://medium.com/@foo',
+    guid: 'https://medium.com/p/foohash',
+    author: 'Foobar da Silva',
+    thumbnail: '',
+    description: 'mock description',
+    content: 'foo content',
+    enclosure: [],
+    categories: []
+  }]
 
-	const expectedArray = [{
-		title: 'mock title',
-		date: '2022-01-01 17:14:14',
-		link: 'https://medium.com/@foo',
-		categories: []
-	}]
+  const expectedArray = [{
+    title: 'mock title',
+    date: '2022-01-01 17:14:14',
+    link: 'https://medium.com/@foo',
+    categories: []
+  }]
 
-	const result = privates._sanitizePostList(mockArray)
+  const result = privates._sanitizePostList(mockArray)
 
-	t.true(JSON.stringify(result) === JSON.stringify(expectedArray), 'json is properly sanitized')
+  t.true(JSON.stringify(result) === JSON.stringify(expectedArray), 'json is properly sanitized')
 })
